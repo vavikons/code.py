8125fa5c979235c3c61d7d2223fe676b8bec5da8
Konstan <vavikons@mail.ru>
2021-01-21 20:29:16 +0300

    v0.2

--- code.py
+++ code.py
@@ -6,8 +6,7 @@ import sys
 def load_image(name):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        fullname = os.path.join('data', 'not_found.png')
     image = pygame.image.load(fullname)
     image = image.convert_alpha()
     return image
